e8152b95f4ae57630f700678bb437205d369cef1
Jason Brooks <jbrooks@redhat.com>
2016-12-09 10:02:46 -0800

    typo

--- kubelet/launch.sh
+++ kubelet/launch.sh
@@ -5,4 +5,4 @@ source /etc/kubernetes/config
 
 ARGS=$(echo "$@ $KUBE_LOGTOSTDERR $KUBE_LOG_LEVEL $KUBELET_API_SERVER $KUBELET_ADDRESS $KUBELET_PORT $KUBELET_HOSTNAME $KUBE_ALLOW_PRIV $KUBELET_ARGS" | xargs -n1 | sort -u -t = -k 1,1 | xargs)
 
-exec /usr/bin/kube-apiserver $ARGS --containerized
+exec /usr/bin/kubelet $ARGS --containerized
